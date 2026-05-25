c453d572bf071e6af24495a542f75976c4c2fc16
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-25 10:55:52 +0700

    Refactor API handler to use constants for config

--- api/status.js
+++ api/status.js
@@ -1,34 +1,36 @@
 export default async function handler(req, res) {
 
-    try {
+  const DOMAIN_SUNTIK = 'https://fayupedia.id';
+  const API_ID = '64416';
+  const API_KEY = 'ahnqh6-su5urz-ip5bom-im5cy3-9qc2n4';
 
-        const {
-            id
-        } = req.query;
+  try {
 
-        const response = await fetch("https://fayupedia.id/api/status", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            body: new URLSearchParams({
-                api_id: "64416",
-                api_key: "ahnqh6-su5urz-ip5bom-im5cy3-9qc2n4",
-                id
-            })
-        });
+    const { id } = req.query;
 
-        const data = await response.json();
+    const response = await fetch(`${DOMAIN_SUNTIK}/api/status`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        api_id: API_ID,
+        api_key: API_KEY,
+        id
+      })
+    });
 
-        res.status(200).json(data);
+    const data = await response.json();
 
-    } catch (err) {
+    res.status(200).json(data);
 
-        res.status(500).json({
-            status: false,
-            message: err.message
-        });
+  } catch (e) {
 
-    }
+    res.status(500).json({
+      success: false,
+      message: e.message
+    });
+
+  }
 
 }
