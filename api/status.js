a080f205e8c8f6a68418098b22ae7d6699f152ef
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-25 10:18:25 +0700

    Update API endpoint and credentials in status.js

---
+++ api/status.js
@@ -0,0 +1,34 @@
+export default async function handler(req, res) {
+
+    try {
+
+        const {
+            id
+        } = req.query;
+
+        const response = await fetch("https://fayupedia.id/api/status", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: new URLSearchParams({
+                api_id: "64416",
+                api_key: "ahnqh6-su5urz-ip5bom-im5cy3-9qc2n4",
+                id
+            })
+        });
+
+        const data = await response.json();
+
+        res.status(200).json(data);
+
+    } catch (err) {
+
+        res.status(500).json({
+            status: false,
+            message: err.message
+        });
+
+    }
+
+}
