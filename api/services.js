0c84a2569d83aec836d5f6b96afc0ec8e5395afa
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-25 10:13:36 +0700

    Add handler function for API service requests

---
+++ api/services.js
@@ -0,0 +1,29 @@
+export default async function handler(req, res) {
+
+    try {
+
+        const response = await fetch("https://fayupedia.id/api/services", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: new URLSearchParams({
+                api_id: "64416",
+                api_key: "ahnqh6-su5urz-ip5bom-im5cy3-9qc2n4"
+            })
+        });
+
+        const data = await response.json();
+
+        res.status(200).json(data);
+
+    } catch (err) {
+
+        res.status(500).json({
+            status: false,
+            message: err.message
+        });
+
+    }
+
+}
