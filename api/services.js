226b3c9eb6f05eb4d85a7a04c65619b4362a28ef
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-25 10:53:20 +0700

    Refactor API handler for improved readability

--- api/services.js
+++ api/services.js
@@ -1,33 +1,33 @@
 export default async function handler(req, res) {
 
-    try {
+  const DOMAIN_SUNTIK = 'https://fayupedia.id';
+  const API_ID = '64416';
+  const API_KEY = 'ahnqh6-su5urz-ip5bom-im5cy3-9qc2n4';
 
-        const response = await fetch("https://fayupedia.id/api/services", {
+  try {
 
-            method: "POST",
+    const response = await fetch(`${DOMAIN_SUNTIK}/api/services`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        api_id: API_ID,
+        api_key: API_KEY
+      })
+    });
 
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
+    const data = await response.json();
 
-            body: new URLSearchParams({
-                api_id: "64416",
-                api_key: "ahnqh6-su5urz-ip5bom-im5cy3-9qc2n4"
-            })
+    res.status(200).json(data);
 
-        });
+  } catch (e) {
 
-        const data = await response.json();
+    res.status(500).json({
+      success: false,
+      message: e.message
+    });
 
-        res.status(200).json(data);
-
-    } catch (e) {
-
-        res.status(500).json({
-            status: false,
-            message: e.message
-        });
-
-    }
+  }
 
 }
